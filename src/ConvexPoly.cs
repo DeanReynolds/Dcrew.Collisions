054644f62c2f0bc72f1b7f47ce6b90e7aad36ce7
Dcrew <dean.reynolds@live.co.uk>
2020-10-24 17:08:56 +0100

    add line & convexpoly

---
+++ src/ConvexPoly.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Dcrew.Collisions {
+    public class ConvexPoly {
+        public Vector2[] Verts;
+        public Vector2 XY;
+        public float Rotation {
+            get => _rotation;
+            set {
+                for (var i = 0; i < Verts.Length; i++) {
+                    var p = Verts[i];
+                    float cos = MathF.Cos(-_rotation),
+                        sin = MathF.Sin(-_rotation),
+                        x = p.X,
+                        y = p.Y,
+                        xcos = x * cos,
+                        ycos = y * cos,
+                        xsin = x * sin,
+                        ysin = y * sin;
+                    p = Verts[i] = new Vector2(xcos - ysin, xsin + ycos);
+                    cos = MathF.Cos(value);
+                    sin = MathF.Sin(value);
+                    x = p.X;
+                    y = p.Y;
+                    xcos = x * cos;
+                    ycos = y * cos;
+                    xsin = x * sin;
+                    ysin = y * sin;
+                    Verts[i] = new Vector2(xcos - ysin, xsin + ycos);
+                }
+                _rotation = value;
+            }
+        }
+
+        float _rotation;
+
+        public Vector2 Center {
+            get {
+                var v = Vector2.Zero;
+                for (var i = 0; i < Verts.Length; i++)
+                    v += Verts[i];
+                return (v / Verts.Length) + XY;
+            }
+        }
+
+        public ConvexPoly(params Vector2[] verts) {
+            Verts = verts;
+        }
+
+        public virtual Vector2 Clamp(Vector2 xy) {
+            var doesIntersect = true;
+            var close = Vector2.Zero;
+            var dist = float.MaxValue;
+            var vert = Verts[0] + XY;
+            var firstVert = vert;
+            Line line;
+            Vector2 iClose;
+            for (var i = 1; i < Verts.Length; i++) {
+                var nextVert = Verts[i] + XY;
+                line = new Line(vert, nextVert);
+                if (!Line.IsLeft(line.A, line.B, xy))
+                    doesIntersect = false;
+                iClose = line.ClosestPoint(xy);
+                float iDist = Vector2.DistanceSquared(iClose, xy);
+                if (iDist < dist) {
+                    close = iClose;
+                    dist = iDist;
+                }
+                vert = nextVert;
+            }
+            line = new Line(vert, firstVert);
+            if (!Line.IsLeft(line.A, line.B, xy))
+                doesIntersect = false;
+            if (doesIntersect)
+                return xy;
+            iClose = line.ClosestPoint(xy);
+            if (Vector2.DistanceSquared(iClose, xy) < dist)
+                close = iClose;
+            return close;
+        }
+
+        public virtual bool Contains(Vector2 xy) {
+            var vert = Verts[0] + XY;
+            var firstVert = vert;
+            for (var i = 1; i < Verts.Length; i++) {
+                var nextVert = Verts[i] + XY;
+                if (!Line.IsLeft(vert, nextVert, xy))
+                    return false;
+                vert = nextVert;
+            }
+            if (!Line.IsLeft(vert, firstVert, xy))
+                return false;
+            return true;
+        }
+        public bool Intersects(ConvexPoly value) {
+            for (var i = 0; i < Verts.Length; i++)
+                if (Contains(value.Clamp(Verts[i] + XY)))
+                    return true;
+            for (var i = 0; i < value.Verts.Length; i++)
+                if (value.Contains(Clamp(value.Verts[i] + value.XY)))
+                    return true;
+            return false;
+        }
+    }
+}
---
+++ src/Line.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Dcrew.Collisions {
+    public struct Line {
+        public Vector2 A, B;
+
+        public Line(Vector2 a, Vector2 b) {
+            A = a;
+            B = b;
+        }
+
+        public bool Intersects(Line line, out Vector2 intersection) {
+            (float X, float Y) n1 = (B.X - A.X, B.Y - A.Y);
+            (float X, float Y) = (line.B.X - line.A.X, line.B.Y - line.A.Y);
+            var denominator = n1.X * Y - n1.Y * X;
+            if (MathF.Abs(denominator) < float.Epsilon) {
+                intersection = Vector2.Zero;
+                return false;
+            }
+            (float X, float Y) n3 = (line.A.X - A.X, line.A.Y - A.Y);
+            var t = (n3.X * Y - n3.Y * X) / denominator;
+            if (t < 0 || t > 1) {
+                intersection = Vector2.Zero;
+                return false;
+            }
+            var u = (n3.X * n1.Y - n3.Y * n1.X) / denominator;
+            if (u < 0 || u > 1) {
+                intersection = Vector2.Zero;
+                return false;
+            }
+            intersection = new Vector2(A.X + t * n1.X, A.Y + t * n1.Y);
+            return true;
+        }
+        public bool Intersects(Vector2 xy, float radiusSquared) => Vector2.DistanceSquared(xy, ClosestPoint(xy)) <= radiusSquared;
+
+        public Vector2 ClosestPoint(Vector2 p) {
+            var ab = B - A;
+            var distance = Vector2.Dot(p - A, ab) / ab.LengthSquared();
+            return distance < 0 ? A : distance > 1 ? B : A + ab * distance;
+        }
+        public static bool IsLeft(Vector2 a, Vector2 b, Vector2 p) => (b.X - a.X) * (p.Y - a.Y) - (p.X - a.X) * (b.Y - a.Y) > 0;
+    }
+}
